0e8fb9bf2b725a1439edbe8f3a0e734e7257052d
Jonathan Wong <jonathanfwong@gmail.com>
2022-06-15 18:40:24 +1000

    another commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,7 +8,6 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
-    // ...add more providers here
   ],
   callbacks: {
     async session({session, token}) {
